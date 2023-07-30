29ad4355211d20eb49c78c057c8073834a4ca65d
Mike Pratt <mike@msjp.me>
2023-07-30 16:12:24 +0100

    Initial commit - added initial implementtion for lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,63 @@
+package lexer
+
+import (
+	"monkey/token"
+)
+
+type Lexer struct {
+  input           string
+  position        int // currnet position in input (current character)
+  readPosition    int // current reading position in input (after current char)
+  ch              byte // current character under evaluation
+}
+
+func New(input string) *Lexer {
+  lexer := &Lexer{input: input}
+  lexer.readChar()
+  return lexer
+}
+
+// Reads the next character in the input sequence, sets the lexer to 0 if we reach the end.
+func (lexer *Lexer) readChar() {
+  if lexer.readPosition >= len(lexer.input) {
+    lexer.ch = 0
+  } else {
+    lexer.ch = lexer.input[lexer.readPosition]
+  }
+
+  lexer.position = lexer.readPosition
+  lexer.readPosition += 1
+}
+
+func (lexer *Lexer) NextToken() token.Token {
+  var tok token.Token
+
+  switch lexer.ch  {
+    case '=':
+      tok = newToken(token.ASSIGN,lexer.ch) 
+    case ';':
+      tok = newToken(token.SEMICOLON, lexer.ch) 
+    case '(':
+      tok = newToken(token.LPAREN, lexer.ch) 
+    case ')':
+      tok = newToken(token.RPAREN, lexer.ch) 
+    case ',':
+      tok = newToken(token.COMMA, lexer.ch) 
+    case '+':
+      tok = newToken(token.PLUS, lexer.ch) 
+    case '{':
+      tok = newToken(token.LBRACE, lexer.ch) 
+    case '}':
+      tok = newToken(token.RBRACE, lexer.ch)
+    case 0:
+      tok.Literal = ""
+      tok.Type = token.EOF
+  }
+
+  lexer.readChar()
+  return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+  return token.Token{Type: tokenType, Literal: string(ch)}
+}
---
+++ token/token.go
@@ -0,0 +1,33 @@
+package token
+
+type TokenType string
+
+type Token struct {
+  Type    TokenType
+  Literal string
+}
+
+const (
+  ILLEGAL   = "ILLEGAL" // tokens/characters we don't know about/are undefined.
+  EOF       = "EOF" // End of file
+
+  // Variable identifiers and literal values
+  IDENT     = "IDENT" // variable identifiers, foo, bar, user, etc.
+  INT       = "INT"   // 123, 3555
+  
+  // Operators
+  ASSIGN    = "="
+  PLUS      = "+"
+  MINUS     = "-"
+
+  // Delimitters
+  COMMA     = ","
+  SEMICOLON = ";"
+  LPAREN    = "("
+  RPAREN    = ")"
+  LBRACE    = "{"
+  RBRACE    = "}"
+
+  FUNCTION  = "FUNCTION"
+  LET       = "LET"
+)
