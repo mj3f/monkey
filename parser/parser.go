9ce1a1997feba666b6168d9097826339cf56dbd4
Mike Pratt <mike@msjp.me>
2023-08-10 22:14:05 +0100

    Added unit tests for testing let statements in the parser. Added parse let statements to the parsers parseProgram function

--- parser/parser.go
+++ parser/parser.go
@@ -28,5 +28,63 @@ func (parser *Parser) nextToken() {
 }
 
 func (parser *Parser) ParseProgram() *ast.Program {
-  return nil
+  program := &ast.Program{}
+  program.Statements = []ast.Statement{}
+
+  for !parser.currentTokenIs(token.EOF) {
+    statement := parser.parseStatement()
+    if statement != nil {
+      program.Statements = append(program.Statements, statement)
+    }
+    parser.nextToken()
+  }
+
+  return program
+}
+
+func (parser *Parser) parseStatement() ast.Statement {
+  switch parser.currentToken.Type {
+  case token.LET:
+    return parser.parseLetStatement()
+  default:
+    return nil
+  }
+}
+
+func (parser *Parser) parseLetStatement() *ast.LetStatement {
+  statement := &ast.LetStatement{Token: parser.currentToken}
+
+  if !parser.expectPeek(token.IDENT) {
+    return nil
+  }
+
+  statement.Name = &ast.Identifier{Token: parser.currentToken, Value: parser.currentToken.Literal}
+
+  if !parser.expectPeek(token.ASSIGN) {
+    return nil
+  }
+
+  // We're skipping the epxrssions until we encounter a semicolon
+  for !parser.currentTokenIs(token.SEMICOLON) {
+    parser.nextToken()
+  }
+
+  return statement
+}
+
+func (parser *Parser) currentTokenIs(tokenType token.TokenType) bool {
+  return parser.currentToken.Type == tokenType
+}
+
+func (parser *Parser) peekTokenIs(tokenType token.TokenType) bool {
+  return parser.peekToken.Type == tokenType
+}
+
+func (parser *Parser) expectPeek(tokenType token.TokenType) bool {
+  if parser.peekTokenIs(tokenType) {
+    parser.nextToken()
+    return true
+  }
+
+  return false
 }
