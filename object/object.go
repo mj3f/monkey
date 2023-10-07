e817b5c016e91b2ae013e1d880bd3a1bb0752b1a
Mike Pratt <mike@msjp.me>
2023-10-07 17:13:32 +0100

    added error handling

--- object/object.go
+++ object/object.go
@@ -9,6 +9,7 @@ const (
 	BOOLEAN_OBJ 			= "BOOLEAN"
 	NULL_OBJ 					= "NULL"
 	RETURN_VALUE_OBJ 	=	"RETURN"
+	ERROR_OBJ 				= "ERROR"
 )
 
 type Object interface {
@@ -60,4 +61,16 @@ func (rv *ReturnValue) Inspect() string {
 
 func (rv *ReturnValue) Type() ObjectType {
 	return RETURN_VALUE_OBJ
+}
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Inspect() string {
+	return "ERROR: " + e.Message
+}
+
+func (e *Error) Type() ObjectType {
+	return ERROR_OBJ
 }
